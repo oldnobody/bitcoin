6e91114a1f172d048850e761008effa0a21e900e
Oliver Grimm <olivergrimm@fastmail.fm>
2019-01-08 13:41:04 +0900

    julia bitcoin checker

--- bitcoin_price_checker.jl
+++ bitcoin_price_checker.jl
@@ -4,38 +4,46 @@ using HTTP
 using JSON
 
 url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
-pricefile = "lastprice.cvs"
+pricefile = "lastprice.csv"
 
-function getPrice(url::String)::String
+function getPrice(url::String)::Float64
   r = HTTP.request("GET", url )
   s = String(r.body)
   j = JSON.parse(s)
-  price = j["bpi"]["USD"]["rate"]
-  return price
+  p = j["bpi"]["USD"]["rate"]
+  p = parse(Float64,replace(p, "," => ""))
+  p = round(p, digits=2)
+  return p
 end
 
-# get the price from url
-price = getPrice(url)
-floatprice = parse(Float64,replace(price, "," => ""))
-
 function getLastPrice(file::String)::Float64
-  lastprice = 0
-
+  l = 0
   if isfile(file)
     f = open(file, "r")
     p = readlines(f)[1]
-    lastprice = parse(Float64,replace(p, "," => ""))
+    l = parse(Float64,p)
+    println("Last price was $l USD")
     close(f)
-  else
-    f = open(file, "w")
-    write(f,price)
-    close(f)
-    println("Writing lastprice.cvs")
   end
-
-  return lastprice
+  return l
 end
 
-getLastPrice(pricefile)
-println(string("Bitcoin is currently at ", floatprice, " USD"))
+function savePrice(file::String, price::Float64)
+  f = open(file, "w")
+  write(f,string(price))
+  close(f)
+end
+
+price = getPrice(url)
+last = getLastPrice(pricefile)
+difference = round(price - last, digits=2)
+
+if (last != 0) && (abs(difference) > 0.0)
+  direction = difference > 0.0 ? "up" : "down"
+  println("Bitcoin is currently at $price USD, $direction $(abs(difference)) USD")
+else
+  println("Bitcoin is currently at $price USD")
+end
+
+savePrice(pricefile, price)
 
